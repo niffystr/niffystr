11cb07e9e54a310b71a8b20e46d7523daccf1d68
niffystr <nomoyajowo@gmail.com>
2025-10-04 11:21:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("login-form")?.addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert("Login feature coming soon! Google integration will be added later.");
+});
